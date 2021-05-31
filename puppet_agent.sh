ac788638c139be213a06e38273a3d42823b10970
PrathyushaTumuluri <84500969+PrathyushaTumuluri@users.noreply.github.com>
2021-05-31 14:40:18 +0530

    add file to create and configure puppet agent on slave node

---
+++ puppet_agent.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# run as sudo
+export HISTIGNORE='*sudo -S*';
+sudo -S -k -i <<< $sudo_pwd;
+
+# set source
+source /etc/profile.d/puppet-agent.sh
+
+# export path
+export PATH=/opt/puppetlabs/bin:$PATH
+    
+# puppet version
+/opt/puppetlabs/bin/puppet -V
+
+## set config
+#/opt/puppetlabs/bin/puppet config set server puppet --section main
+
+# set config from file
+cat ./configfile.txt > /etc/puppetlabs/puppet/puppet.conf
+# echo "[main]\nserver = puppet\ndns_alt_names=ip-172-31-31-109.ap-south-1.compute.internal,ip-172-31-29-129.ap-south-1.compute.internal" > /etc/puppetlabs/puppet/puppet.conf
+
+# run ssl bootstrap
+/opt/puppetlabs/bin/puppet ssl bootstrap
