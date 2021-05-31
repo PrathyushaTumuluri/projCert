3c213ed9799d6f607f0019f21823047963cde2d8
PrathyushaTumuluri <84500969+PrathyushaTumuluri@users.noreply.github.com>
2021-05-31 18:30:28 +0530

    Create puppet_master_sign.sh

---
+++ puppet_master_sign.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+sleep 10;
+
+echo "get certs list";
+sudo -i puppetserver ca list;
+
+echo "export cert name";
+export certname=$(sudo -i echo $(sudo -i puppetserver ca list | grep -o DNS:kslave1.* | sed 's/DNS://' | sed 's/"]//'));
+echo $certname;
+
+# echo "sign certificate";
+# sudo -i puppetserver ca sign --certname $certname;
+
+# echo "list signed certs";
+# sudo -i puppetserver ca list -a;
