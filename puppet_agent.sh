14a4cd1acb984a95f2773770cd64cc47cb762ddc
PrathyushaTumuluri <84500969+PrathyushaTumuluri@users.noreply.github.com>
2021-05-31 17:31:14 +0530

    Update puppet_agent.sh

--- puppet_agent.sh
+++ puppet_agent.sh
@@ -7,23 +7,23 @@
 #sudo -S -k -i <<< "DevOps12";
 
 # set source
-sudo -i -u root source /etc/profile.d/puppet-agent.sh;
+sudo -i source /etc/profile.d/puppet-agent.sh;
 
 # export path
-sudo -i -u root export PATH=/opt/puppetlabs/bin:$PATH;
+sudo -i export PATH=/opt/puppetlabs/bin:$PATH;
     
 # puppet version
-sudo -i -u root /opt/puppetlabs/bin/puppet -V;
+sudo -i /opt/puppetlabs/bin/puppet -V;
 
 # # set config
 # sudo -i /opt/puppetlabs/bin/puppet config set server puppet --section main;
 
 # set config from file
-sudo -i -u root cat ./configfile.txt > /etc/puppetlabs/puppet/puppet.conf;
+sudo -i -u edureka cat ./configfile.txt > /etc/puppetlabs/puppet/puppet.conf;
 #echo "[main]\nserver = puppet\ndns_alt_names=ip-172-31-31-109.ap-south-1.compute.internal,ip-172-31-29-129.ap-south-1.compute.internal" > /etc/puppetlabs/puppet/puppet.conf
 
 # verify the set config
-sudo -i -u root cat /etc/puppetlabs/puppet/puppet.conf;
+sudo -i cat /etc/puppetlabs/puppet/puppet.conf;
 
 # # run ssl bootstrap
 # /opt/puppetlabs/bin/puppet ssl bootstrap
