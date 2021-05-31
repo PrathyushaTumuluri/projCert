ec7ec75cf16f6a54c1d00ad2932db2086e0be457
PrathyushaTumuluri <84500969+PrathyushaTumuluri@users.noreply.github.com>
2021-05-31 18:28:21 +0530

    Create puppet_agent.sh

---
+++ puppet_agent.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+echo "set source";
+sudo -i source /etc/profile.d/puppet-agent.sh;
+
+echo "export path";
+sudo -i export PATH=/opt/puppetlabs/bin:$PATH;
+    
+# echo "puppet version";
+# sudo -i /opt/puppetlabs/bin/puppet -V;
+
+echo "set config";
+sudo -i /opt/puppetlabs/bin/puppet config set server puppet --section main;
+sudo -i /opt/puppetlabs/bin/puppet config set dns_alt_names ip-172-31-31-109.ap-south-1.compute.internal,ip-172-31-29-129.ap-south-1.compute.internal --section main;
+
+echo "verify the set config";
+sudo -i cat /etc/puppetlabs/puppet/puppet.conf;
+
+echo "run ssl bootstrap";
+sudo -i /opt/puppetlabs/bin/puppet ssl bootstrap &
