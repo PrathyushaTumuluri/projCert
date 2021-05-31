28ba01cf1246cd48ecff132fdb6f7dac5f44e091
PrathyushaTumuluri <84500969+PrathyushaTumuluri@users.noreply.github.com>
2021-05-31 17:04:13 +0530

    Update puppet_agent.sh

--- puppet_agent.sh
+++ puppet_agent.sh
@@ -1,29 +1,29 @@
 #!/bin/bash
 
 # run as sudo
-export HISTIGNORE='*sudo -S*';
-sudo -i;
+#export HISTIGNORE='*sudo -S*';
+#sudo -i;
 #sudo -S -k -i
 #sudo -S -k -i <<< "DevOps12";
 
 # set source
-sudo -i source /etc/profile.d/puppet-agent.sh;
+sudo -i -u root source /etc/profile.d/puppet-agent.sh;
 
 # export path
-sudo -i export PATH=/opt/puppetlabs/bin:$PATH;
+sudo -i -u root export PATH=/opt/puppetlabs/bin:$PATH;
     
 # puppet version
-sudo -i /opt/puppetlabs/bin/puppet -V;
+sudo -i -u root /opt/puppetlabs/bin/puppet -V;
 
 ## set config
 #sudo -i /opt/puppetlabs/bin/puppet config set server puppet --section main
 
 # set config from file
-sudo -i cat ./configfile.txt > /etc/puppetlabs/puppet/puppet.conf;
+sudo -i -u root cat ./configfile.txt > /etc/puppetlabs/puppet/puppet.conf;
 # echo "[main]\nserver = puppet\ndns_alt_names=ip-172-31-31-109.ap-south-1.compute.internal,ip-172-31-29-129.ap-south-1.compute.internal" > /etc/puppetlabs/puppet/puppet.conf
 
 # verify the set config
-sudo -i cat /etc/puppetlabs/puppet/puppet.conf;
+sudo -i -u root cat /etc/puppetlabs/puppet/puppet.conf;
 
 # # run ssl bootstrap
 # /opt/puppetlabs/bin/puppet ssl bootstrap
