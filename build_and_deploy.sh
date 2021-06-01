078d6c86653e2f9674ff5ccda1f1be44ee169b01
PrathyushaTumuluri <84500969+PrathyushaTumuluri@users.noreply.github.com>
2021-06-01 13:48:16 +0530

    Update build_and_deploy.sh

--- build_and_deploy.sh
+++ build_and_deploy.sh
@@ -7,5 +7,5 @@ echo "run docker image"
 sudo docker run -d -p 8092:80 php-website-edureka:v1.0.0
 
 echo "test curl"
-curl_result=$(sudo curl -v http://localhost:8092/ 2>&1)
+curl_result=$(sudo curl -v http://kslave1:8092/ 2>&1)
 echo $curl_result
