a6bba1b3a3e198713604e02b5d3f965bd6334580
PrathyushaTumuluri <84500969+PrathyushaTumuluri@users.noreply.github.com>
2021-06-01 13:43:07 +0530

    Update build_and_deploy.sh

--- build_and_deploy.sh
+++ build_and_deploy.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 echo "build docker image"
-docker build -t php-website-edureka:v1.0.0 .
+sudo docker build -t php-website-edureka:v1.0.0 .
 
 echo "run docker image"
-docker run -d -p 8092:80 php-website-edureka:v1.0.0
+sudo docker run -d -p 8092:80 php-website-edureka:v1.0.0
 
 echo "test curl"
 curl_result=$(curl -v http://localhost:8092/ 2>&1)
