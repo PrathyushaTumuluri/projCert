d9a5afcae3f234cc5037553f29c87c73beb85017
PrathyushaTumuluri <84500969+PrathyushaTumuluri@users.noreply.github.com>
2021-06-03 16:53:07 +0530

    Update docker_manifest.sh

--- docker_manifest.sh
+++ docker_manifest.sh
@@ -1,28 +1,21 @@
 #!/bin/bash
 
-echo "present working directory:";
-pwd;
+echo "cd to puppet modules...";
+cd /etc/puppetlabs/code/environments/production/modules;
 
-# echo "cd to puppet modules...";
-# cd /etc/puppetlabs/code/environments/production/modules;
+# echo "present working directory:";
+# pwd;
 
-# # echo "present working directory:";
-# # pwd;
+echo "install garethr-docker module...";
+sudo /opt/puppetlabs/bin/puppet module install garethr-docker;
 
-# # sudo /opt/puppetlabs/bin/puppet module install garethr-docker
+echo "create manifest file...";
+sudo touch /etc/puppetlabs/code/environments/production/manifests/installdocker.pp;
 
-# echo "install garethr-docker module...";
-# sudo /opt/puppetlabs/bin/puppet module install garethr-docker;
+sudo chmod 777 /etc/puppetlabs/code/environments/production/manifests/installdocker.pp;
 
-# # cd /etc/puppetlabs/code/environments/production/manifests
+echo "add code to manifest file...";
+sudo -i cat var/lib/jenkins/workspacecreate_manifest_for_docker/puppet_docker_class.txt > /etc/puppetlabs/code/environments/production/manifests/installdocker.pp;
 
-# echo "create manifest file...";
-# sudo touch /etc/puppetlabs/code/environments/production/manifests/installdocker.pp;
-
-# sudo chmod 777 /etc/puppetlabs/code/environments/production/manifests/installdocker.pp;
-
-# echo "add code to manifest file...";
-# sudo -i cat ./puppet_docker_class.txt > /etc/puppetlabs/code/environments/production/manifests/installdocker.pp;
-
-# echo "verify manifest file...";
-# sudo cat /etc/puppetlabs/code/environments/production/manifests/installdocker.pp;
+echo "verify manifest file...";
+sudo cat /etc/puppetlabs/code/environments/production/manifests/installdocker.pp;
