0402170e6b5204828ceeaeebeed8bb03ea2232fe
PrathyushaTumuluri <84500969+PrathyushaTumuluri@users.noreply.github.com>
2021-06-03 18:04:17 +0530

    Update puppet_master_sign.sh

--- puppet_master_sign.sh
+++ puppet_master_sign.sh
@@ -9,12 +9,19 @@ echo "export cert name";
 export certname=$(sudo -i echo $(sudo -i puppetserver ca list | grep -o kslave1.*.internal));
 echo $certname;
 
-if [ "$certname" == *"No certificates to list"* ]; then
-  echo "No certificates to sign..";
+if grep -q "No certificates to list" <<< $certname; then
+  echo "No certificates to sign...";
 else
   echo "sign certificate";
   sudo -i puppetserver ca sign --certname $certname;
 fi
+
+# if [ "$certname" == *"No certificates to list"* ]; then
+#   echo "No certificates to sign..";
+# else
+#   echo "sign certificate";
+#   sudo -i puppetserver ca sign --certname $certname;
+# fi
 
 echo "list signed certs";
 sudo -i puppetserver ca list -a;
