57357a902d2bfba68dc41f926cfd504a5e38c950
PrathyushaTumuluri <84500969+PrathyushaTumuluri@users.noreply.github.com>
2021-06-03 16:34:01 +0530

    Update docker_manifest.sh

--- docker_manifest.sh
+++ docker_manifest.sh
@@ -17,7 +17,7 @@ echo "create manifest file...";
 sudo touch /etc/puppetlabs/code/environments/production/manifests/installdocker.pp;
 
 echo "add code to manifest file...";
-sudo -i cat ./puppet_docker_class.txt > /etc/puppetlabs/code/environments/production/manifests/installdocker.pp;
+sudo sh -c "cat ./puppet_docker_class.txt > /etc/puppetlabs/code/environments/production/manifests/installdocker.pp";
 
 echo "verify manifest file...";
 sudo cat /etc/puppetlabs/code/environments/production/manifests/installdocker.pp;
